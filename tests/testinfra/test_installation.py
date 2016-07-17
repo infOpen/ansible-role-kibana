d5a523a7cd1a35760ac39758ff32dddfee29a907
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-07-17 13:45:33 +0200

    Add install management

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -5,12 +5,36 @@ import pytest
 
 
 # To run all the tests on given docker images:
-pytestmark = pytest.mark.docker_images('infopen/ubuntu-xenial-ssh-py27')
+pytestmark = pytest.mark.docker_images('infopen/ubuntu-xenial-ssh-py27:0.2.0')
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+def test_packages(Package):
+    """
+    Tests about packages installed on all systems
+    """
+
+    packages = [
+        'python-apt-common', 'python-apt', 'kibana'
+    ]
+
+    for package in packages:
+        assert Package(package).is_installed is True
 
 
-def test_foo_b(User):
-    assert User().name == 'root'
+def test_process(Process):
+    """
+    Test about kibana processus
+    """
+
+    assert len(Process.filter(user='kibana')) == 1
+
+
+def test_service(Command, Service, Socket):
+    """
+    Test about kibana service
+    """
+
+    assert Service('kibana').is_enabled
+    assert Service('kibana').is_running
+    assert Command('systemctl status kibana').rc == 0
+    assert Socket("tcp://0.0.0.0:5601").is_listening
